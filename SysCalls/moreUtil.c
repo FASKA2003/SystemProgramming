1500ed2e9a59a2bab52d61c6f602a84b7a06b345
= <ayoubelmahfoudi05@gmail.com>
2023-12-15 16:43:12 +0100

    Add a simple implemetation of the linux more utility.

--- SysCalls/moreUtil.c
+++ SysCalls/moreUtil.c
@@ -2,11 +2,15 @@
 #include <stdlib.h>
 
 #define MAX_LEN 512
+#define PAGE_LEN 20
+
+int get_input(FILE *);
 void do_more(FILE *file);
 int main(int argc, char *argv[]) {
   if (argc == 1) {
-    fprintf(stdout, "Not enough args!!!\n");
-    exit(0);
+    do_more(stdin);
+    // fprintf(stdout, "Not enough args!!!\n");
+    // exit(0);
   }
   int i = 0;
   FILE *fp;
@@ -23,8 +27,42 @@ int main(int argc, char *argv[]) {
 }
 
 void do_more(FILE *file) {
+  int num_of_lines = 0;
+  int rv;
   char buffer[MAX_LEN];
+  FILE *fp_tty = fopen("/dev//tty", "r");
   while (fgets(buffer, MAX_LEN, file)) {
     fputs(buffer, stdout);
+    num_of_lines++;
+    if (num_of_lines == PAGE_LEN) {
+      rv = get_input(fp_tty);
+      if (rv == 0) {
+        printf("\033[1A \033[2K \033[1G");
+        break;
+      } else if (rv == 1) {
+        printf("\033[1A \033[2K \033[1G");
+        num_of_lines -= PAGE_LEN;
+      } else if (rv == 2) {
+        printf("\033[1A \033[2K \033[1G");
+        num_of_lines -= 1;
+      } else {
+        break;
+      }
+    }
+  }
+}
+
+int get_input(FILE *cmdstream) {
+  printf("\033[7m --more--(%%) \033[m");
+  int c = getc(cmdstream);
+  switch (c) {
+  case 'q':
+    return 0;
+  case ' ':
+    return 1;
+  case '\n':
+    return 2;
+  default:
+    return 3;
   }
 }
--- SysCalls/opensys.c
+++ SysCalls/opensys.c
@@ -1,30 +1,33 @@
 #include <errno.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <syscall.h>
-#include <string.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <syscall.h>
+#include <unistd.h>
 
-int main()
-{
+int main() {
   int fd;
-  char buffer[] = "/home/itsmeayoub/Documents/Coding/System_Programming/file";
-  char text[] = "This file is used by the user itsmeayoub:AYOUB EL MAHFOUDI to store some useful stuff.\n";
+  char buffer[] = "/home/itsmeayoub/Documents/Coding/System_Programming/file\n";
+  char text[] = "This file is used by the user itsmeayoub:AYOUB EL MAHFOUDI to "
+                "store some useful stuff.\n";
   fd = open(buffer, O_RDWR | O_APPEND);
-  if (fd >= 0)
-  {
-    fprintf(stdout,"The file is opened successfully, The file descriptor is %d.\n",fd);
+  if (fd >= 0) {
+    fprintf(stdout,
+            "The file is opened successfully, The file descriptor is %d.\n",
+            fd);
   } else {
-    fprintf(stdout,"An error has happened while trying to open the file, the error code is %d.\n",errno);
+    fprintf(stdout,
+            "An error has happened while trying to open the file, the error "
+            "code is %d.\n",
+            errno);
     exit(errno);
   }
   int bytes = write(fd, text, sizeof(text));
-  if (bytes > 0)
-  {
-    fprintf(stdout,"The number of bytes written is %d.\n",bytes);
+  if (bytes > 0) {
+    fprintf(stdout, "The number of bytes written is %d.\n", bytes);
   } else {
-    fprintf(stdout,"The write operation didnot successed.\n");
+    fprintf(stdout, "The write operation didnot successed.\n");
   }
   close(fd);
 }
